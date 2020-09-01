d1f46b8c8748022a78a33216deb4d72de15e0362
Flemeg Amorim <flemeg@gmail.com>
2020-08-31 21:36:26 -0300

    update cors config

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,18 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid-v4')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+    projectId: 'cloneinsta-8ee25',
+    keyFilename: 'keyfile.json'
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  
-  
+    functions.logger.info("Hello logs!", { structuredData: true });
+
+
 });
