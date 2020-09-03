04ff301e718ee2867b04373cccdf14d655fab8ef
Flemeg Amorim <flemeg@gmail.com>
2020-09-02 22:20:37 -0300

    implemented cloud functions

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@ const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true })
 const fs = require('fs')
 const uuid = require('uuid-v4')
-const { Storage } = require('@google-cloud/storage')
+const { Storage } = require('@google-cloud/storage');
+const { request } = require('http');
 const storage = new Storage({
     projectId: 'cloneinsta-8ee25',
     keyFilename: 'keyfile.json'
@@ -13,6 +14,37 @@ const storage = new Storage({
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
+})
 
-
-});
+exports.uploadImage = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+        try {
+            fs.writeFileSync('/tmp/imageToSave.jpg', request.body.image, 'base64')
+            const bucket = storage.bucket('cloneinsta-8ee25.appspot.com')
+            const id = uuid()
+            bucket.upload('/tmp/imageToSave.jpg', {
+                uploadType: 'media',
+                destination: `/posts/${id}.jpg`,
+                metadata: {
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        firebaseStorageDownloadTokens: id,
+                    }
+                }
+            }, (err, file) => {
+                if (err) {
+                    console.log(err)
+                    return response.status(500).json({ error: err })
+                } else {
+                    const fileName = encodeURIComponent(file.name)
+                    const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/' +
+                        bucket.name + '/o/' + fileName + '?alt=media&token=' + id
+                    return response.status(201).json({ imageUrl })
+                }
+            })
+        } catch (err) {
+            console.log(err)
+            return response.status(500).json({ error: err })
+        }
+    })
+})
